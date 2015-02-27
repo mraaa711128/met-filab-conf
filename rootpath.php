032d872bfa99b2ca15de6cef8e2f0dd28cf440c7
Stanley <mraaa711128@gmail.com>
2015-02-26 23:13:35 -0500

    newfeature/newfrontimage
    FrontPage:
        New Image
        Add Presentation Button
        Submission change to dropdown menu
    RootPath:
        Add support DNS

--- rootpath.php
+++ rootpath.php
@@ -1,6 +1,15 @@
 <?php 
 	define('DS', DIRECTORY_SEPARATOR);
 
+	// only use for on-line
+	// $serverhost = $_SERVER['HTTP_HOST'];
+	// if (substr_compare($serverhost,"www",0,3,false) == 0) {
+	// 	# code...
+	// 	$siteroot = "http://" . $serverhost . DS . "conference";
+	// } else {
+	// 	$siteroot = "http://" . $serverhost;
+	// }
+
 	$siteroot = "//localhost/met-filab-conf";
 	$pageroot = "/Users/mraaa711128/ownCloud/Project/Web/met-filab-conf";
 ?>
