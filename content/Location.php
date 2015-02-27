a28df91af00292a7fbede0592a5d7cfa2206105a
Stanley <mraaa711128@gmail.com>
2015-02-27 05:58:06 -0500

    bugfix/locationwordwrap
    Location:
    	Description can wrap auto
    	Map can resize auto

---
+++ content/Location.php
@@ -0,0 +1,42 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="page-header">Location</h1>
+    <!--<p>We are still working on preparing a fatastic conference for you. Come back for checking our latest program and schedule oftenly.</p>-->
+    <!--<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>-->
+  </div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-9">
+			<div class="col-md-8">
+				<div class="container-fluid">
+					<iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Metropol%20Lake%20Resort%2C%20Ohrid%2C%20Macedonia%20(FYROM)&zoom=15&language=en-us&key=AIzaSyAoTla3xGHrT7FMSbzLXCv30uyO0jvGeiA"></iframe>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="page-header">
+					<h4>Location Detail</h4>
+				</div>
+				<div class="container-fluid">
+					<h5>Title:</h5>Metropol Lake Resort &nbsp&nbsp&nbsp<a href="http://www.metropol-ohrid.com.mk/EN/default.aspx" target="_blank">more...</a>
+				</div>
+				<div class="container-fluid">
+					<h5>Address:</h5>Settlment Konjsko B.B. 6000 Ohrid, Macedonia
+				</div>
+				<div class="container-fluid">
+					<h5>Phone No:</h5>+389 (46) 203001
+				</div>
+				<div class="container-fluid">
+					<h5>Fax No:</h5>+389 (46) 277212
+				</div>
+				<div class="page-header">
+					<h4>Transportation</h4>
+				</div>
+				<div class="container-fluid">
+					<h5>Bus:</h5>(XXX)XXXXXXX - XXXXXXX
+					<h5>Subway:</h5>Red Line to XXXXXXXX Station
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
